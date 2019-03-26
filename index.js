b4bd6f0c1b2fecc617322585727d1ccde8042471
Johannes Tegnér <johannes@jitesoft.com>
2019-03-26 10:25:04 +0100

    More fixes.

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const helperUtils = require('@babel/helper-plugin-utils');
 const declare = helperUtils['declare'];
 
-module.exports = declare((api) => {
+module.exports = declare((api, options) => {
     api.assertVersion(7);
+
+    const {
+        modules,
+        mode
+    } = options;
+
+    let {
+        targets
+    } = options;
+
+    if (targets === undefined) {
+        targets = '> 0.25%, not dead, node >= 8';
+        if (mode === 'web') {
+            targets = '> 0.25%, not dead'
+        } else if (mode === 'node') {
+            targets = 'node >= 8'
+        }
+    }
 
     return {
         'presets': [
@@ -10,25 +28,15 @@ module.exports = declare((api) => {
                 require('@babel/preset-env'),
                 {
                     useBuiltIns: 'usage',
-                    corejs: {version: 3, proposals: true},
-                    targets: '> 0.25%, not dead, node > 8',
-                    spec: false,
-                    loose: false,
-                    modules: 'auto',
-                    debug: false,
-                    include: [],
-                    exclude: [],
-                    forceAllTransforms: false,
-                    configPath: process.cwd(),
-                    ignoreBrowserslistConfig: false,
-                    shippedProposals: false
+                    corejs: {version: 3, proposals: false},
+                    targets: targets,
+                    modules: modules === undefined ? 'auto' : modules
                 }
             ]
         ],
         'plugins': [
             [
                 require('@babel/plugin-proposal-object-rest-spread'), {
-                    loose: false,
                     useBuiltIns: true
                 }
             ],
@@ -40,45 +48,15 @@ module.exports = declare((api) => {
             ],
             [
                 require('@babel/plugin-transform-runtime'), {
-                    absoluteRuntime: false,
-                    corejs: {version: 3, proposals: true},
-                    helpers: true,
-                    regenerator: true,
-                    useESModules: true // Webpack fixes this.
+                    corejs: {version: 3, proposals: true}
                 }
             ],
-            [
-                require('@babel/plugin-proposal-partial-application'), {
-
-                }
-            ],
-            [
-                require('@babel/plugin-proposal-class-properties'), {
-                    loose: false
-                }
-            ],
-            [
-                require('@babel/plugin-proposal-optional-chaining'), {
-                    loose: false
-                }
-            ],
-            [
-                require('@babel/plugin-proposal-throw-expressions'), {
-                    asyncGenerators: true,
-                    generators: true,
-                    async: true
-                }
-            ],
-            [
-                require('@babel/plugin-proposal-nullish-coalescing-operator'), {
-                    loose: false
-                }
-            ],
-            [
-                require('@babel/plugin-proposal-private-methods'), {
-                    loose: false
-                }
-            ]
+            require('@babel/plugin-proposal-partial-application'),
+            require('@babel/plugin-proposal-class-properties'),
+            require('@babel/plugin-proposal-optional-chaining'),
+            require('@babel/plugin-proposal-throw-expressions'),
+            require('@babel/plugin-proposal-nullish-coalescing-operator'),
+            require('@babel/plugin-proposal-private-methods')
         ]
     }
 });
