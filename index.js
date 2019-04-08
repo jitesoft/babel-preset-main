14bd1d8e58c7599b1b32a1288b5b9896deb5869f
Johannes Tegnér <johannes@jitesoft.com>
2019-04-08 10:50:16 +0200

    Added ability to exclude plugins and define corejs version.

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ module.exports = declare((api, options) => {
     api.assertVersion(7);
 
     const {
+        exclude,
         modules,
-        mode
+        mode,
+        corejs,
     } = options;
 
     let {
@@ -22,42 +24,59 @@ module.exports = declare((api, options) => {
         }
     }
 
+    const isExcluded = (str, or) => {
+        if ((exclude || []).indexOf(str) === -1) {
+            return or();
+        }
+        return null;
+    };
+
+
     return {
         'presets': [
-            [
+            (isExcluded('@babel/preset-env', () => [
                 require('@babel/preset-env'),
                 {
                     useBuiltIns: 'usage',
-                    corejs: {version: 3, proposals: false},
+                    corejs: {version: corejs || 3, proposals: false},
                     targets: targets,
                     modules: modules === undefined ? 'auto' : modules
                 }
-            ]
-        ],
+            ]))
+        ].filter(p => p !== null),
         'plugins': [
-            [
+            isExcluded('@babel/plugin-proposal-object-rest-spread', () => [
                 require('@babel/plugin-proposal-object-rest-spread'), {
                     useBuiltIns: true
                 }
-            ],
-            [
+            ]),
+            isExcluded('@babel/plugin-proposal-decorators', () => [
                 require('@babel/plugin-proposal-decorators'), {
                     decoratorsBeforeExport: true,
                     legacy: false
                 }
-            ],
-            [
+            ]),
+            isExcluded('@babel/plugin-transform-runtime', () => [
                 require('@babel/plugin-transform-runtime'), {
-                    corejs: {version: 3, proposals: true}
+                    corejs: {version: corejs || 3, proposals: true}
                 }
-            ],
-            require('@babel/plugin-proposal-partial-application'),
-            require('@babel/plugin-proposal-class-properties'),
-            require('@babel/plugin-proposal-optional-chaining'),
-            require('@babel/plugin-proposal-throw-expressions'),
-            require('@babel/plugin-proposal-nullish-coalescing-operator'),
-            require('@babel/plugin-proposal-private-methods'),
-            require('@babel/plugin-syntax-dynamic-import')
-        ]
+            ]),
+            isExcluded('@babel/plugin-proposal-partial-application', () =>
+                require('@babel/plugin-proposal-partial-application')),
+            isExcluded('@babel/plugin-proposal-class-properties', () =>
+                require('@babel/plugin-proposal-class-properties')),
+            isExcluded('@babel/plugin-proposal-optional-chaining', () =>
+                require('@babel/plugin-proposal-optional-chaining')),
+            isExcluded('@babel/plugin-proposal-throw-expressions', () =>
+                require('@babel/plugin-proposal-throw-expressions')),
+            isExcluded('@babel/plugin-proposal-nullish-coalescing-operator', () =>
+                require('@babel/plugin-proposal-nullish-coalescing-operator')),
+            isExcluded('@babel/plugin-proposal-private-methods', () =>
+                require('@babel/plugin-proposal-private-methods')),
+            isExcluded('@babel/plugin-syntax-dynamic-import', () =>
+                require('@babel/plugin-syntax-dynamic-import')),
+            isExcluded('@babel/plugin-proposal-export-default-from', () =>
+                require('@babel/plugin-proposal-export-default-from'))
+        ].filter(p => p !== null)
     }
 });
