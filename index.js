dc72dcf5b2810684fc8b5fac6f202d30dd57ed55
Johannes Tegnér <johannes@jitesoft.com>
2019-12-30 12:07:29 +0100

    Default node version now 12 (latest LTS). Made it possible to exclude with just plugin name (i.e., without @babel/plugin-...). Also did some cleanup.

--- index.js
+++ index.js
@@ -3,31 +3,23 @@ const declare = helperUtils['declare'];
 
 const defaultTargets = {
   web: 'defaults',
-  node: 'node >= 10'
+  node: 'node >= 12'
 };
 
 module.exports = declare((api, options) => {
   api.assertVersion(7);
-
-  const {
-    exclude,
-    mode
-  } = options;
-
-  let {
-    targets
-  } = options;
+  let targets = options.targets;
 
   //region Helpers.
-  const isExcluded = (str, or) => ((exclude || []).indexOf(str) === -1) ? or() : null;
+  const isExcluded = (str, or) => ((options.exclude || []).indexOf(str) === -1) ? or() : null;
   const setOr = (value, or) => value === undefined ? or : value;
   //endregion
 
-  if (targets === undefined) {
+  if (!targets) {
     targets = `${defaultTargets.web}, ${defaultTargets.node}`;
-    if (mode === 'web') {
+    if (options.mode === 'web') {
       targets = defaultTargets.web;
-    } else if (mode === 'node') {
+    } else if (options.mode === 'node') {
       targets = defaultTargets.node;
     }
   }
@@ -48,24 +40,24 @@ module.exports = declare((api, options) => {
 
   return {
     'presets': [
-      (isExcluded('@babel/preset-env', () => [
+      (isExcluded('preset-env', () => [
         require('@babel/preset-env'),
         defaultPresetEnv
       ]))
     ].filter(p => p !== null),
     'plugins': [
-      isExcluded('@babel/plugin-proposal-object-rest-spread', () => [
+      isExcluded('object-rest-spread', () => [
         require('@babel/plugin-proposal-object-rest-spread'), {
           useBuiltIns: defaultPresetEnv.useBuiltIns
         }
       ]),
-      isExcluded('@babel/plugin-proposal-decorators', () => [
+      isExcluded('decorators', () => [
         require('@babel/plugin-proposal-decorators'), {
           decoratorsBeforeExport: setOr(options.decoratorsBeforeExport, true),
           legacy: setOr(options.legacy, false)
         }
       ]),
-      isExcluded('@babel/plugin-transform-runtime', () => [
+      isExcluded('transform-runtime', () => [
         require('@babel/plugin-transform-runtime'), {
           corejs: {
             version: 3,
@@ -73,35 +65,35 @@ module.exports = declare((api, options) => {
           }
         }
       ]),
-      isExcluded('@babel/plugin-proposal-partial-application', () =>
+      isExcluded('partial-application', () =>
         require('@babel/plugin-proposal-partial-application')),
-      isExcluded('@babel/plugin-proposal-class-properties', () =>
+      isExcluded('class-properties', () =>
         require('@babel/plugin-proposal-class-properties')),
-      isExcluded('@babel/plugin-proposal-optional-chaining', () =>
+      isExcluded('optional-chaining', () =>
         require('@babel/plugin-proposal-optional-chaining')),
-      isExcluded('@babel/plugin-proposal-throw-expressions', () =>
+      isExcluded('throw-expressions', () =>
         require('@babel/plugin-proposal-throw-expressions')),
-      isExcluded('@babel/plugin-proposal-nullish-coalescing-operator', () =>
+      isExcluded('nullish-coalescing-operator', () =>
         require('@babel/plugin-proposal-nullish-coalescing-operator')),
-      isExcluded('@babel/plugin-proposal-private-methods', () =>
+      isExcluded('private-methods', () =>
         require('@babel/plugin-proposal-private-methods')),
-      isExcluded('@babel/plugin-syntax-dynamic-import', () =>
+      isExcluded('dynamic-import', () =>
         require('@babel/plugin-syntax-dynamic-import')),
-      isExcluded('@babel/plugin-proposal-export-default-from', () =>
+      isExcluded('export-default-from', () =>
         require('@babel/plugin-proposal-export-default-from')),
-      isExcluded('@babel/plugin-proposal-export-namespace-from', () =>
+      isExcluded('export-namespace-from', () =>
         require('@babel/plugin-proposal-export-namespace-from')),
-      isExcluded('@babel/plugin-proposal-async-generator-functions', () =>
+      isExcluded('async-generator-functions', () =>
         require('@babel/plugin-proposal-async-generator-functions')),
-      isExcluded('@babel/plugin-transform-named-capturing-groups-regex', () =>
+      isExcluded('named-capturing-groups-regex', () =>
         require('@babel/plugin-transform-named-capturing-groups-regex')),
-      isExcluded('@babel/plugin-transform-dotall-regex', () =>
+      isExcluded('dotall-regex', () =>
         require('@babel/plugin-transform-dotall-regex')),
-      isExcluded('@babel/plugin-transform-object-assign', () =>
+      isExcluded('object-assign', () =>
         require('@babel/plugin-transform-object-assign')),
-      isExcluded('@babel/plugin-proposal-optional-catch-binding', () =>
+      isExcluded('optional-catch-binding', () =>
         require('@babel/plugin-proposal-optional-catch-binding')),
-      isExcluded('@babel/plugin-transform-for-of',
+      isExcluded('for-of',
         () => require('@babel/plugin-transform-for-of'))
     ].filter(p => p !== null)
   };
